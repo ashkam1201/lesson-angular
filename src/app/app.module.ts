78479652901ce0c996017b1955d98223c03a6894
Adrian Hagjija <adrianhagjija@hotmail.com>
2021-09-03 03:34:24 +0200

    routing part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,19 +7,33 @@ import { MonPremierComponent } from './mon-premier/mon-premier.component';
 import { AppareilComponent } from './appareil/appareil.component';
 
 import {AppareilService} from "./services/appareil.service";
+import { AuthComponent } from './auth/auth.component';
+import { AppareilViewComponent } from './appareil-view/appareil-view.component';
+import {RouterModule, Routes} from "@angular/router";
+import {AuthService} from "./services/auth.service";
+
+const appRoutes: Routes = [
+  {path: 'appareils', component: AppareilViewComponent },
+  {path: 'auth', component: AuthComponent},
+  {path: '', component: AppareilViewComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     MonPremierComponent,
-    AppareilComponent
+    AppareilComponent,
+    AuthComponent,
+    AppareilViewComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [
-    AppareilService
+    AppareilService,
+    AuthService
   ],
   bootstrap: [AppComponent]
 })
