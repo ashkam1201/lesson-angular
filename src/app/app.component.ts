a86c6da7b39d8c9d00497f6c3f938bdc57ba89ca
Adrian Hagjija <adrianhagjija@hotmail.com>
2021-09-06 17:07:10 +0200

    Router pt.2 and RXJS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,28 @@
-import { Component} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/interval';
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  constructor() {
+export class AppComponent implements OnInit, OnDestroy {
+
+  secondes: number;
+  countersubscription: Subscription;
+
+  ngOnInit() {
+    const counter = Observable.interval(1000);
+    this.countersubscription = counter.subscribe(
+      (value: number) => {
+        this.secondes = value;
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    this.countersubscription.unsubscribe();
   }
 }
-
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,11 +11,19 @@ import { AuthComponent } from './auth/auth.component';
 import { AppareilViewComponent } from './appareil-view/appareil-view.component';
 import {RouterModule, Routes} from "@angular/router";
 import {AuthService} from "./services/auth.service";
+import { SingleAppareilComponent } from './single-appareil/single-appareil.component';
+import { FourOhFourComponent } from './four-oh-four/four-oh-four.component';
+import {AuthGuard} from "./services/auth-guard.service";
+import { EditAppareilComponent } from './edit-appareil/edit-appareil.component';
 
 const appRoutes: Routes = [
-  {path: 'appareils', component: AppareilViewComponent },
+  {path: 'appareils', canActivate: [AuthGuard], component: AppareilViewComponent},
+  {path: 'appareils/:id', canActivate: [AuthGuard], component: SingleAppareilComponent},
+  {path: 'edit', canActivate: [AuthGuard], component: EditAppareilComponent},
   {path: 'auth', component: AuthComponent},
-  {path: '', component: AppareilViewComponent}
+  {path: '', component: AppareilViewComponent},
+  {path: 'not-found', component: FourOhFourComponent},
+  {path: '**', redirectTo: '/not-found' }
 ];
 
 @NgModule({
@@ -24,7 +32,10 @@ const appRoutes: Routes = [
     MonPremierComponent,
     AppareilComponent,
     AuthComponent,
-    AppareilViewComponent
+    AppareilViewComponent,
+    SingleAppareilComponent,
+    FourOhFourComponent,
+    EditAppareilComponent
   ],
   imports: [
     BrowserModule,
@@ -33,7 +44,8 @@ const appRoutes: Routes = [
   ],
   providers: [
     AppareilService,
-    AuthService
+    AuthService,
+    AuthGuard
   ],
   bootstrap: [AppComponent]
 })
